c30ddad59ea77084be3a53253f0f540231e4e084
hlasensky <tomas.hlasensky@seznam.cz>
2023-12-08 14:22:49 +0100

    AOC 3. day done

--- 3.js
+++ 3.js
@@ -2,37 +2,71 @@ import reader from "./reader.js";
 
 let arr = reader("./3.txt");
 
-let nmLen = 0;
-
 const maskOne = [
 	[-1, +1],
 	[0, +1],
 	[+1, +1],
 	[-1, 0],
-	// [0, 0],
 	[+1, 0],
 	[-1, -1],
 	[0, -1],
 	[+1, -1],
 ];
 
-const res = []
+const res = [];
+let nm = "";
+let isValid = [];
+let gearArr = []
+
 
 arr.forEach((line, lineIndex) => {
 	line.forEach((char, charIndex) => {
-		if (Number(char)) {
-			maskOne.forEach((cords) => {
-                if (lineIndex + cords[ 1 ] >= 0 && charIndex + cords[ 0 ] >= 0 &&
-                    lineIndex + cords[ 1 ] < arr.length && charIndex + cords[ 0 ] < line.length)
-                {
-                    if (Number(arr[lineIndex + cords[1]][charIndex + cords[0]])) nmLen += 1;
-                    console.log(arr[ lineIndex + cords[ 1 ] ][ charIndex + cords[ 0 ] ]);
-                    res.push(arr[lineIndex + cords[1]][charIndex + cords[0]])
+		if (Number(char) || Number(char) === 0) {
+			nm += char;
+			for (let cordIndex = 0; cordIndex < maskOne.length; cordIndex++) {
+				if (
+					lineIndex + maskOne[cordIndex][1] >= 0 &&
+					charIndex + maskOne[cordIndex][0] >= 0 &&
+					lineIndex + maskOne[cordIndex][1] < arr.length &&
+					charIndex + maskOne[cordIndex][0] < line.length
+				) {
+					let ch =
+						arr[lineIndex + maskOne[cordIndex][1]][charIndex + maskOne[cordIndex][0]];
+					if (ch !== "." && Number(ch) !== 0 && !Number(ch)) {
+						isValid = [
+							lineIndex + maskOne[cordIndex][1],
+							charIndex + maskOne[cordIndex][0],
+						];
+					}
 				}
-			});
+			}
+		}
+		if (!(Number(char) || Number(char) === 0) || charIndex === line.length - 1) {
+			if (isValid.length) {
+				res.push(Number(nm));
+				if (arr[isValid[0]][isValid[1]] === "*") {
+					gearArr.push([nm, isValid]);
+				}
+				isValid = [];
+			}
+			nm = "";
 		}
 	});
 });
 
-console.log(nmLen);
-console.log(res);
+gearArr = gearArr.filter((item) => item);
+
+gearArr = gearArr.map((item, ind) => {
+	return gearArr.map((it, i) =>
+		item[1].toString() === it[1].toString() && i !== ind ? item[0] * it[0] : 0
+	);
+});
+
+let cnt = 0;
+let cntG = 0;
+res.forEach((item) => (cnt += item));
+gearArr = gearArr.map((item) => item.filter(nm => nm));
+
+gearArr.forEach((item) => Number(cntG += item[0] ? item[0] : 0));
+console.log(cnt);
+console.log(cntG/2);
